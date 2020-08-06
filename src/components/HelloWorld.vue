4c0733381e136d86fc3dabef203f8e692d174ea6
ljil <randomlycoding@gmail.com>
2020-08-06 20:23:11 +0400

    Загрузка таблицы из django

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,7 +41,7 @@
       <!-- Без выделения цветом -->
       <template v-slot:item.salary="{ item }">
         <!-- Редактирование Ставки:salary fraction -->
-        <v-edit-dialog pa-6 :return-value.sync="item.salary">
+        <v-edit-dialog :return-value.sync="item.salary">
           {{item.salary}}₽ ({{item.fraction}})%
           <template v-slot:input>
             <v-container>
@@ -115,6 +115,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -133,98 +135,32 @@ export default {
         { text: "Почасовая", value: "byHours" },
       ],
       data: [],
-      pre_data: [
-        {
-          name: "Джордж Вашингтон",
-          companyName: 'ООО "Синергия"',
-          positionName: "Первый президент США",
-          hireDate: "1789-04-30",
-          fireDate: "1797-03-04",
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: true,
-        },
-        {
-          name: "Ричард I Львиное Сердце",
-          companyName: 'ООО "Синергия"',
-          positionName: "Король Англии",
-          hireDate: "1189-01-01",
-          fireDate: "1199-05-17",
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: true,
-        },
-        {
-          name: "Джейсон Стейтем",
-          companyName: 'ООО "Синергия"',
-          positionName: "Бейкон",
-          hireDate: "1998-09-01",
-          fireDate: null,
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: false,
-        },
-        {
-          name: "Тарантино К. Дж.",
-          companyName: 'ООО "Синергия"',
-          positionName: "Джимми",
-          hireDate: "1994-04-15",
-          fireDate: null,
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: false,
-        },
-        {
-          name: "Камбербэтч Б.",
-          companyName: 'ООО "Синергия"',
-          positionName: "Смауг",
-          hireDate: "1000-01-01",
-          fireDate: "2941-10-09",
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: false,
-        },
-        {
-          name: "Крузенштерн И. Ф.",
-          companyName: 'ООО "Синергия"',
-          positionName: "Человек и пароход",
-          hireDate: "1770-11-08",
-          fireDate: null,
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: false,
-        },
-        {
-          name: "Бендер С. Р.",
-          companyName: '"Planet Express"',
-          positionName: "Робот-сгибальщик",
-          hireDate: "2997-03-27",
-          fireDate: null,
-          salary: 1000,
-          fraction: 100,
-          base: 1000,
-          advance: 1000,
-          byHours: true,
-        },
-      ],
+      pre_data: [],
     };
   },
   mounted() {
-    this.showFired();
+    this.loadData();
   },
   methods: {
+    async loadData() {
+      this.pre_data = await axios.post("http://localhost:8000/api/", {
+        query: `{
+          getOccupations{
+            id
+            name
+            companyName
+            positionName
+            hireDate
+            fireDate
+            salary
+            base
+            fraction
+            byHours
+            }
+            }`,
+      });
+      this.showFired();
+    },
     filterNames(value, search, item) {
       return item.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
     },
@@ -233,12 +169,15 @@ export default {
     },
     showFired() {
       if (this.fired) {
-        this.data = this.pre_data;
+        this.data = this.pre_data.data.data.getOccupations;
       } else {
-        this.data = this.pre_data.filter(function (entry) {
+        this.data = this.pre_data.data.data.getOccupations.filter(function (
+          entry
+        ) {
           return entry.fireDate === null;
         });
       }
+      ``;
     },
   },
 };
